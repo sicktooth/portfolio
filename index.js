0c5619841aadff1834bc9d05b2702984a763562e
sicktooth <sosaronungwe@gmail.com>
2023-12-24 10:41:36 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var myName = ("Samuel");
 var yourName = prompt ("What is your name?");
-alert ("My name is " + myName + ", welcome to my world " + yourName + "😃");
+alert ("My name is " + myName + ", welcome to my world " + yourName + " 😃");

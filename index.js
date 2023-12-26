c16c1fe4ae8bdc594e6c8acfcefd6c23eba8bbad
sicktooth <sosaronungwe@gmail.com>
2023-12-26 02:53:52 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var myName = ("Samuel");
 var yourName = prompt ("What is your name?");
-alert ("My name is " + myName + ", welcome to my world " + yourName + " 😃");
+var CapFName = yourName.slice(0,1);
+CapFName = CapFName.toUpperCase();
+var RestName = yourName.slice(1, yourName.length);
+RestName = RestName.toLowerCase();
+var YourName = CapFName + RestName;
+alert ("My name is " + myName + ", welcome to my world " + YourName + " 😃");
